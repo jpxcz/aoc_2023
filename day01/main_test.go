bb67d067bda6f2785fded72d77b64d53463408ad
Jose Flores <jop0693@gmail.com>
2023-12-01 10:25:37 -0600

    d01 test object change

--- day01/main_test.go
+++ day01/main_test.go
@@ -3,16 +3,11 @@ package main
 import "testing"
 
 func TestPartOneTotal(t *testing.T) {
-	lines := make([]string, 4)
-	lines[0] = "1abc2"
-	lines[1] = "pqr3stu8vwx"
-	lines[2] = "a1b2c3d4e5f"
-	lines[3] = "treb7uchet"
 	file := struct {
 		lines []string
 		total int
 	}{
-		lines: lines,
+		lines: []string{"1abc2", "pqr3stu8vwx", "a1b2c3d4e5f", "treb7uchet"},
 		total: 142,
 	}
 
@@ -25,21 +20,20 @@ func TestPartOneTotal(t *testing.T) {
 	}
 }
 
-
 func TestPartTwoTotal(t *testing.T) {
-	lines := make([]string, 7)
-	lines[0] = "two1nine"
-	lines[1] = "eightwothree"
-	lines[2] = "abcone2threexyz"
-	lines[3] = "xtwone3four"
-	lines[4] = "4nineeightseven2"
-	lines[5] = "zoneight234"
-	lines[6] = "7pqrstsixteen"
 	file := struct {
 		lines []string
 		total int
 	}{
-		lines: lines,
+		lines: []string{
+			"two1nine",
+			"eightwothree",
+			"abcone2threexyz",
+			"xtwone3four",
+			"4nineeightseven2",
+			"zoneight234",
+			"7pqrstsixteen",
+		},
 		total: 281,
 	}
 
