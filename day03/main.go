a76cbb716632a3d71695cc0ff417bf415e6d1180
Jose Flores <jop0693@gmail.com>
2023-12-04 17:37:01 -0600

    day03 part1 refactor

--- day03/main.go
+++ day03/main.go
@@ -5,8 +5,6 @@ import (
 	"fmt"
 	"os"
 	"strconv"
-	"strings"
-	"unicode"
 )
 
 type TextScan []string
@@ -33,71 +31,107 @@ func main() {
 	}
 
 	partOne(text)
+}
 
+type NumberCoordinates struct {
+	value  int
+	x      int
+	y      int
+	yEnd   int
+	len    int
+	parsed bool
+}
+
+func NewNumberCoordinates(x, y, yEnd int, numberString string) *NumberCoordinates {
+	n, err := strconv.Atoi(numberString)
+	if err != nil {
+		panic(err)
+	}
+
+	return &NumberCoordinates{
+		value:  n,
+		x:      x,
+		y:      y,
+		yEnd:   yEnd,
+		len:    len(numberString),
+		parsed: false,
+	}
+}
+
+func getNumberCoordinates(text TextScan) []*NumberCoordinates {
+	numbers := make([]*NumberCoordinates, 0)
+
+	for lIndex, line := range text {
+		numberStart := -1
+		numberEnd := -1
+		for rIndex, r := range line {
+			if r >= '0' && r <= '9' {
+				if numberStart == -1 {
+					numberStart = rIndex
+					numberEnd = rIndex
+				} else {
+					numberEnd = rIndex
+				}
+			} else {
+				if numberStart != -1 {
+					n := NewNumberCoordinates(lIndex, numberStart, numberEnd, line[numberStart:numberEnd+1])
+					numbers = append(numbers, n)
+				}
+				numberStart = -1
+				numberEnd = -1
+			}
+		}
+
+		if numberStart != -1 {
+			n := NewNumberCoordinates(lIndex, numberStart, numberEnd, line[numberStart:numberEnd+1])
+			numbers = append(numbers, n)
+		}
+	}
+
+	return numbers
+}
+
+func isSymbol(r rune) bool {
+	if r >= '0' && r <= '9' || r == '.' {
+		return false
+	}
+
+	return true
+}
+
+func findAdjacentNumbers(x, y int, numbers []*NumberCoordinates) int {
+	adjacents := 0
+	for _, n := range numbers {
+		if n.parsed {
+			continue
+		}
+
+		if n.x >= x-1 && n.x <= x+1 && ((n.y >= y-1 && n.y <= y+1) || (n.yEnd >= y-1 && n.yEnd <= y+1)) {
+			fmt.Printf("value=%d\n", n.value)
+			adjacents = adjacents + n.value
+			n.parsed = true
+		}
+	}
+
+	return adjacents
+}
+
+func findAdjacentSymbolsSumatory(text TextScan, numbers []*NumberCoordinates) int {
+	total := 0
+	for lIndex, line := range text {
+		for rIndex, r := range line {
+			if isSymbol(r) {
+				total = total + findAdjacentNumbers(lIndex, rIndex, numbers)
+			}
+		}
+	}
+
+	return total
 }
 
 func partOne(text TextScan) {
-	total := 0
-	for lIndex, line := range text {
-		initRange := 0
-		lastRange := 0
-		digit := false
-		for tIndex, t := range line {
-			if unicode.IsDigit(t) {
-				if !digit {
-					initRange = tIndex
-				}
-				digit = true
-				lastRange = tIndex + 1
-			} else {
-				if digit {
-					total = total + validateMatrix(initRange, lastRange, text, lIndex)
-				}
-
-				digit = false
-				initRange = tIndex
-				lastRange = tIndex
-			}
-
-			if digit && tIndex == len(line) -1 {
-					total = total + validateMatrix(initRange, lastRange, text, lIndex)
-			}
-		}
-	}
-
+	numbers := getNumberCoordinates(text)
+	total := findAdjacentSymbolsSumatory(text, numbers)
 	fmt.Printf("total=%d\n", total)
-}
 
-func hasSymbol(chars string) bool {
-	return strings.ContainsAny(chars, "@#$%^&*()-_=+[]|/")
-}
-
-func validateMatrix(initialRange int, finalRange int, text TextScan, currLine int) int {
-	number := text[currLine][initialRange:finalRange]
-	if initialRange > 0 {
-		initialRange = initialRange - 1
-	}
-
-	if finalRange < len(text[0])-2 {
-		finalRange = finalRange + 1
-
-	}
-
-	for i := currLine - 1; i <= currLine+1; i++ {
-		if i < 0 || i >= len(text) {
-			continue
-		}
-		fmt.Println(i)
-		if hasSymbol(text[i][initialRange:finalRange]) {
-			fmt.Printf("valid number %v\n", number)
-			intVar, err := strconv.Atoi(number)
-			if err != nil {
-				panic(err)
-			}
-
-			return intVar
-		}
-	}
-
-	return 0
 }
--- day03/main_test.go
+++
@@ -1,9 +0,0 @@
-package main
-
-import "testing"
-
-func TestBackLash(t *testing.T) {
-	if !hasSymbol("asdf@") {
-		t.Fatal("expected backlash")
-	}
-}
